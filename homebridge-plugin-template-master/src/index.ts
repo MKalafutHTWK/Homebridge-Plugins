9ae1bb55f6380edb9780d83bf6cb3d08ebd7cdc4
MKalafutHTWK <92427779+MKalafutHTWK@users.noreply.github.com>
2021-11-06 12:39:37 +0100

    Accessory Copy

--- homebridge-plugin-template-master/src/index.ts
+++ homebridge-plugin-template-master/src/index.ts
@@ -15,19 +15,61 @@ let hap: HAP;
 const hours = new Date().getHours;
 
 export = (api: API) => {  //wird aufgerufen, wenn das plugin geladen wird
-  hap = api.hap;
+  hap = api.hap;          //hap Homekit Accessory Protocol
   api.registerAccessory("ExampleSwitchTimer", SwitchTimer);
 };
 
 class SwitchTimer implements AccessoryPlugin{
 
   //private readonly hours: const; 
+  private readonly log: Logging;    //Variablenname: Klasse
   private readonly name: string;
+  private switchOn = false;
 
-  constructor(log: Logging, config: AccessoryConfig, api: API) {
+  private readonly switchService: Service;
+  private readonly informationService: Service;
+
+  constructor(log: Logging, config: AccessoryConfig, api: API) {    //Erzeugt das Objekt SwitchTimer
+    this.log = log;
     this.name = config.name;
     
+    
+    this.switchService = new hap.Service.Switch(this.name);
+    this.switchService.getCharacteristic(hap.Characteristic.On)
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        log.info("Current state of the switch was returned: " + (this.switchOn? "ON": "OFF"));
+        callback(undefined, this.switchOn);
+      })
+      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+        this.switchOn = value as boolean;
+        log.info("Switch state was set to: " + (this.switchOn? "ON": "OFF"));
+        callback();
+      });
+
+    this.informationService = new hap.Service.AccessoryInformation()
+      .setCharacteristic(hap.Characteristic.Manufacturer, "Custom Manufacturer")
+      .setCharacteristic(hap.Characteristic.Model, "Custom Model");
+
+    log.info("Switch finished initializing!");
+  }
+   /*
+   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
+   * Typical this only ever happens at the pairing process.
+   */
+   identify(): void {
+    this.log("Identify!");
   }
 
+  /*
+   * This method is called directly after creation of this instance.
+   * It should return all services which should be added to the accessory.
+   */
+  getServices(): Service[] {
+    return [
+      this.informationService,
+      this.switchService,
+    ];
+  }
 }
+
 
